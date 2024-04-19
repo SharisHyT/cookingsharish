427e1c9a6c51c87bb87c992a612e8235a7ec4db0
SharisHyT <135478569+SharisHyT@users.noreply.github.com>
2024-04-19 23:54:37 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+clear
+pkg update -y
+clear
+pkg upgrade -y
+clear
+
+printf by Sharish
+
+printf установка Java...
+
+pkg install openjdk-17 -y
+
+clear
+
+java -version
+clear
+
+pkg install python -y
+
+pip install subprocess
+pip install os-sys
+
+wget
