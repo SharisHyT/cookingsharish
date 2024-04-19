320df629aaf47de6ce71f22f4ac92d89aff02c21
SharisHyT <135478569+SharisHyT@users.noreply.github.com>
2024-04-20 00:04:21 +0300

    Create s.py

---
+++ s.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+subprocess.run[("clear")]
+
+while True:
+    print("1. Nukkit (1.14-?.??.??)")
+    print("2. PurPur (1.17.1) ")
+    cmd = input("Выберайте ядро: ")
+    
+    if cmd == "1":
+        wget https://github.com/PetteriM1/NukkitPetteriM1Edition/releases/latest
+      print("создаю файл для запуска... (start.sh)")
+      wget
+    elif cmd == "2":
+        curl -OJ https://api.purpurmc.org/v2/purpur/1.17.1/1428/download
+      print("создаю файл для запуска... (start.sh)")
+      wget
+        break
+    else:
+        print("Вы ввели не правильное значение")
